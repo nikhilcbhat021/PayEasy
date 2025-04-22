dbd548b1bdef74792a46768ccc0159c9d3e242f6
Nikhil Bhat <nikhilbhat021@gmail.com>
2025-04-22 18:04:02 +0530

    Added declaration files to import images properly. Happens in next

--- packages/ui/src/hero.tsx
+++ packages/ui/src/hero.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image';
+// import AppBarIcon from '../assets/appbar-icon.png';
 import HeroImg from '../../../apps/user-app/public/home-hero.webp';
 
 const Hero = () => {
   return (
     <div className='w-full flex items-center p-8 gap-8 mx-auto'>
+        {/* <img src={HeroImg} alt='home-hero-img' className='flex flex-row self-end w-3xl mx-auto' /> */}
         <Image 
+            // src='../../../apps/user-app/public/home-hero.webp'
             src={HeroImg}
             alt='home-hero-image'
             className='flex flex-row self-end w-3xl mx-auto'
